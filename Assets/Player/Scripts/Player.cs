0ce58603c62679031d8b1b4a8ff40603fae3226c
Isak <Isakmolvviiing@gmail.com>
2026-02-28 17:18:33 +1300

    Added player functionality

---
+++ Assets/Player/Scripts/Player.cs
@@ -0,0 +1,112 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 movement2D;
+    private Vector3 movement3D;
+    private Vector2 mouse2D;
+    private CharacterController controller;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private Animator animator;
+
+    private float gravity = -9.82f;
+    [SerializeField] private float jumpForce;
+    private float verticalVelocity = -2f;
+    private bool canJump = true;
+
+    private void Update()
+    {
+        ApplyMovement();
+        ApplyGravity();
+       
+    }
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void ReadMovementInput(InputAction.CallbackContext context) // reads players movement direction input
+    {
+        if (context.performed) 
+            movement2D = context.ReadValue<Vector2>();
+
+        if (context.canceled)
+            movement2D = Vector2.zero;
+    }
+
+
+    private void ApplyMovement()
+    {
+        movement3D  = new Vector3(movement2D.x, 0, movement2D.y);
+        Vector3 movementRotation = transform.TransformDirection(movement3D);
+        movementRotation.y = verticalVelocity;
+
+        if (animator)
+        {
+            if (movement2D != Vector2.zero) animator.SetBool("canRun", true);
+            else animator.SetBool("canRun", false);
+        }
+  
+            
+       
+        if (controller)
+            controller.Move(movementRotation * movementSpeed * Time.deltaTime); 
+
+    }
+
+
+    private float yaw = 0; // y
+    private float pitch = 0;  // x
+    public void ReadMouseInput(InputAction.CallbackContext context) // reads players mouse movement input
+    {
+        mouse2D = context.ReadValue<Vector2>();
+
+        yaw += mouse2D.x * mouseSensitivity;
+        pitch -= mouse2D.y * mouseSensitivity;
+
+        pitch = Mathf.Clamp(pitch, -60, 60); // good clamp values for first person.
+
+        if (playerCamera)
+            playerCamera.transform.localRotation = Quaternion.Euler(pitch, 0, 0); // camera rotation used here instead of player so that player does not rotate weirdly when looking up or down 
+        transform.rotation = Quaternion.Euler(0, yaw, 0); // camera is child of player therefore will follow player rotation
+
+    }
+
+    public void ReadJumpInput(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        Debug.Log("Attemping jump...");
+        if (canJump)
+        {
+            verticalVelocity = jumpForce;
+            canJump = false;
+        }
+        
+    }
+
+    [SerializeField] private Transform footLocation;
+    [SerializeField] private LayerMask groundLayer;
+    private void ApplyGravity()
+    {
+        Vector3 endLocation = footLocation.position + Vector3.down * .2f;
+        if (Physics.Linecast(footLocation.position, endLocation, groundLayer))
+        {
+            canJump = true;
+        }
+
+        verticalVelocity += gravity * Time.deltaTime;
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Vector3 endLocation = footLocation.position + (Vector3.down * .2f);
+        Gizmos.DrawLine(footLocation.position, endLocation);
+    }
+}
